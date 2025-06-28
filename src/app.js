9fb156078296c00f64048c0eb7657440581f7da6
UbiquitousKiwi <tstarling25.ts@gmail.com>
2025-06-28 00:00:50 -0400

    removed app-listen

--- src/app.js
+++ src/app.js
@@ -42,9 +42,3 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`); 
-});
-
